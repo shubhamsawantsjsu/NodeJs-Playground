fa2d7ae8803f96f29e3c2edfb0999c62c63cb5b4
Shubham Sawant <shubham.sawant@sjsu.edu>
2018-12-13 16:56:09 -0800

    Committed 17-userclick.js

---
+++ 17-userclick.js
@@ -0,0 +1,24 @@
+function longRunningOperation(callback) {
+    // simulate a 3 second operation
+    setTimeout(callback, 3000);
+}
+
+function userClicked() {
+    console.log('starting a long operation');
+    longRunningOperation(function () {
+        console.log('ending a long operation');
+    });
+}
+
+// simulate a user action
+userClicked();
+userClicked();
+
+
+/*
+Output:
+starting a long operation
+starting a long operation
+ending a long operation
+ending a long operation
+/*
